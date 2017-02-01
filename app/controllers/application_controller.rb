9033d93acf672fe80a61e8786d3ab928f48d9102
Draveness <stark.draven@gmail.com>
2017-02-01 09:16:39 +0800

    Add auth token methods to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  def authenticate
+
+  end
+
+  def authenticate_token
+    authenticate_with_http_token do
+      User.find_by(auth_token: auth_token)
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,30 @@
-class User < ApplicationRecord
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+require 'securerandom'
 
+class User < ApplicationRecord
   before_save { self.email = email.downcase }
+  before_create :set_auth_token
+
   validates :email, presence: true,
                     length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
   has_secure_password
+
+  def reset_auth_token
+    self.auth_token = generate_auth_token
+  end
+
+  private
+
+  def set_auth_token
+    return if auth_token.present?
+    self.auth_token = generate_auth_token
+  end
+
+  def generate_auth_token
+    SecureRandom.uuid.gsub(/\-/, '')
+  end
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 end
